b645345bd1c43ae234f0768368ed3a5fa6b8e08f
johariah <johariah587@gmail.com>
2026-05-21 11:09:10 +0800

    Delete kfc_project directory

--- kfc_project/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem
-from .models import MenuItem, Order, OrderItem
-
-admin.site.register(MenuItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-
